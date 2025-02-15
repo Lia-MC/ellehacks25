42a9f686d4465065d9aa950917c5a1bf21c951d6
xylhub <150547496+xylhub@users.noreply.github.com>
2025-02-15 15:41:31 -0500

    updated home.py

--- 1_Home.py
+++ 1_Home.py
@@ -5,6 +5,19 @@ st.divider()
 
 st.write("Welcome!")
 
+skinDisease = "Acne"
+
+from google import genai
+
+client = genai.Client(api_key="YOUR_API_KEY")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Explain how AI works",
+)
+
+print(response.text)
+
 
 # DATA INPUT
 # name = st.text_input("Your name:")
