afe42d30072167579be6b998bc2b4a8824fdb483
Lia-MC <lia.mc73700@gmail.com>
2025-02-15 16:47:11 -0500

    Added emojis

--- pages/3_Info_Hub.py
+++ pages/3_Info_Hub.py
@@ -6,14 +6,14 @@ st.divider()
 st.write("It's important to care for your health for a happier healthier life!")
 st.write("Here are some tips to make that happen:")
 
-st.header("1. Sleep")
+st.header("1. Sleep :star2:")
 st.write("A good night sleep is essential to keeping to health, including skin health")
 
-st.header("2. Eat healthy")
+st.header("2. Eat healthy :herb:")
 st.write("You are what you eat! Not exactly... but the less oily things you eat, the less skin irritations you will have")
 
-st.header("3. Exercise")
+st.header("3. Exercise :woman-running:")
 st.write("Exercise helps you feel better, mentally and physically")
 
-st.header("4. Practice good hygeine:")
+st.header("4. Practice good hygiene :shower:")
 st.write("It's important to shower and wash your face every night")
