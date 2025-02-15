cb468be84d1d5ad44ebdfcb8570644090df91c67
Lia-MC <lia.mc73700@gmail.com>
2025-02-15 16:13:32 -0500

    Drafted content for info hub page

--- pages/3_Info_Hub.py
+++ pages/3_Info_Hub.py
@@ -2,3 +2,18 @@ import streamlit as st
 
 st.title("Self Care")
 st.divider()
+
+st.write("It's important to care for your health for a happier healthier life!")
+st.write("Here are some tips to make that happen:")
+
+st.header("1. Sleep")
+st.write("A good night sleep is essential to keeping to health, including skin health")
+
+st.header("2. Eat healthy")
+st.write("You are what you eat! Not exactly... but the less oily things you eat, the less skin irritations you will have")
+
+st.header("3. Exercise")
+st.write("Exercise helps you feel better, mentally and physically")
+
+st.header("4. Practice good hygeine:")
+st.write("It's important to shower and wash your face every night")
