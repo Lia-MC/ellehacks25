6ad8ce21e4e954ad5cb3f7ab58984fbcc1091126
Lia-MC <lia.mc73700@gmail.com>
2025-02-15 14:41:56 -0500

    Added home, quotes, info hub, and profile pages

---
+++ 1_Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Home")
+st.divider()
+
+st.write("Welcome!")
+
+
+# DATA INPUT
+# name = st.text_input("Your name:")
+# age = st.text_input("Your age:")
+# skin_conditions = st.text_input("Your skin conditions:")
+
+# DATA RETRIEVAL
+# import pandas as pd 
+# data = pd.read_csv("data.csv")
+# st.write(data)
+
+# MULTIPLE PAGES
+# link_button
---
+++ pages/4_Profile.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("# Your info")
+st.divider()
