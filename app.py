06a533f7a21e2fba668acdc8779458414cfd10b8
xylhub <150547496+xylhub@users.noreply.github.com>
2025-02-16 05:57:09 -0500

    changed display and added new file

---
+++ app.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+
+model = load_model('C:\Users\salix\Downloads\allfiles\Image_Class_Model.ipynb')
+
+img = 'C:\Users\salix\Downloads\allfiles\acne-face-1-15__ProtectWyJQcm90ZWN0Il0_FocusFillWzI5NCwyMjIsIngiLDFd_jpg.rf.b1b13b85a4bbf3106db27ae2fa5c9096.jpg'
+
